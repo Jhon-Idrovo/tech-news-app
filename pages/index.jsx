107a5b11ff0cdb463236b0d286bb4456ba939c54
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-05-09 18:44:16 -0500

    problem not directly solved, trying another approach, focusing on the major objective, not the implementation

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,8 @@ import PostCard from "../components/PostCard";
 import NavBar from "../components/NavBar";
 import Loading from "../components/Loading";
 import { getFirstPosts, getRemainingPosts } from "../lib/news";
+import { data } from "autoprefixer";
+import { unstable_batchedUpdates } from "react-dom";
 
 export default function Home() {
   const [section, setSection] = useState("topstories");
@@ -17,15 +19,24 @@ export default function Home() {
   //in order to show the news fast to the user, the app does not
   //get all the publications in one go, instead, it only show 20 and then
   //gets the remaining as a  side process
+  //the caveat of this approach is that if the user changes sections before
+  // the app renders the remaining props (or even the first ones), when the new section
+  // charges, the content of it is the content that was "queried" on the other section.
+  // This happens because the async function is still running on the back.
+  // An attempt was made trying to verify that the section state was the same as the
+  // section from which we are retrieving the data. The problem is that react updates
+  // the state in batches, so it waits until we update the posts state to set the current section
+  // so we cannot use it on this last proccess
+
   const updatePosts = async (lcSection) => {
     const firstPosts = await getFirstPosts(lcSection);
     if (section === lcSection) {
       console.log("equal");
     }
     setPosts(firstPosts);
-    // const remainingPosts = await getRemainingPosts();
-    // console.log(posts); //this gives null
-    // setPosts([...firstPosts, ...remainingPosts]);
+    const remainingPosts = await getRemainingPosts();
+    console.log(posts); //this gives null
+    setPosts([...firstPosts, ...remainingPosts]);
   };
 
   const changeSection = (newSec) => {
