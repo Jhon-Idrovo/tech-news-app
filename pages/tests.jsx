d6d8f4bd1c75b0db238a3c93f9d7d65d238ff88f
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-05-10 19:42:12 -0500

    axios cancel token implemented, first section renders, when changing it does not

--- pages/tests.jsx
+++ pages/tests.jsx
@@ -1,56 +1,28 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+import { testCancel, testRequest } from "../lib/tests";
+
 export default function Tests() {
-  //   const [pubs, setPubs] = useState(["initial"]);
-  //   useEffect(() => {
-  //     console.log("getting pubs");
-  //     getPubs().then((r) => console.log("done d"));
-  //   }, []);
-  //   useEffect(() => {
-  //     console.log("re-rendering", pubs);
-  //   });
+  const [posts, setPosts] = useState("initial");
 
-  //   const getPubs = async () => {
-  //     console.log("running getPubs");
-  //     const IDs = await axios.get(
-  //       "https://hacker-news.firebaseio.com/v0/topstories.json"
-  //     );
-  //     const choppedIDs = IDs.data.slice(0, 5);
-  //     console.log(choppedIDs);
-  //     const getPubsInLoop = async () => {
-  //       console.log("running loop");
-  //       for (let id of choppedIDs) {
-  //         const pub = await axios.get(
-  //           `https://hacker-news.firebaseio.com/v0/item/${id}.json`
-  //         );
-  //         console.log("setting pubs");
-  //         setPubs((pubs) => [...pubs, pub.data]);
-  //       }
-  //     };
-  //     getPubsInLoop();
-  //     return "done";
-  //   };
+  useEffect(() => {
+    updatePosts();
+    return () => {
+      testCancel();
+    };
+  }, [posts]);
 
-  //   console.log("normal code");
-  //     const getPubsOneGo = async () => {
-  //     for (let id in await axios
-  //       .get("https://hacker-news.firebaseio.com/v0/topstories.json")
-  //       .then((response) => response.data)) {
-  //       console.log(id);
-  //     }
-  //   };
-  //   const p = getPubsOneGo().then((r) => r);
-  //   const getPubs = async () => {
-  //     const IDs = await axios.get(
-  //       `https://hacker-news.firebaseio.com/v0/topstories.json`
-  //     );
-  //     return IDs.data;
-  //   };
-  //   console.log(async () => {
-  //     await getPubs();
-  //   });
+  const updatePosts = async () => {
+    const newPosts = await testRequest();
+    setPosts(newPosts);
+  };
 
-  //   getPubs().then((res) => console.log(res));
-  return <div></div>;
+  return (
+    <div>
+      {posts}
+
+      <button onClick={updatePosts}>change state</button>
+    </div>
+  );
 }
