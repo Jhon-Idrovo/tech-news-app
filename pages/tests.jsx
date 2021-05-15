73be82f033f722945f249bbd460bce3d85b5cb54
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-05-15 07:28:10 -0500

    usePosts created

--- pages/tests.jsx
+++ pages/tests.jsx
@@ -2,27 +2,17 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 import { testCancel, testRequest } from "../lib/tests";
+import usePosts from "../lib/api";
 
 export default function Tests() {
-  const [posts, setPosts] = useState("initial");
+  const [section, setSection] = useState("topstories");
 
-  useEffect(() => {
-    updatePosts();
-    return () => {
-      testCancel();
-    };
-  }, [posts]);
-
-  const updatePosts = async () => {
-    const newPosts = await testRequest();
-    setPosts(newPosts);
-  };
+  const { isLoading, isFetching, posts, updatePosts } = usePosts(section, 20);
 
   return (
     <div>
       {posts}
-
-      <button onClick={updatePosts}>change state</button>
+      <button onClick={updatePosts}>get more</button>
     </div>
   );
 }
